c65a2519248a5923c5a2dda6463d682508354d5a
SpeedCash <tcravero@gmail.com>
2023-12-19 13:00:22 +0000

    the parse file

---
+++ parse.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * parse_line - Divise la ligne de commande en tokens.
+ * @line: La ligne de commande entrée par l'utilisateur.
+ * 
+ * Return: Un tableau de chaînes de caractères, chaque élément étant un token.
+ */
+char **parse_line(char *line)
+{
+    int bufsize = 64, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "hsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, " \t\r\n\a");
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += 64;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "hsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, " \t\r\n\a");
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
