efdf8f37a886b91ecb7ea3a0c580bcc927c6c955
SpeedCash <tcravero@gmail.com>
2023-12-22 08:13:18 +0000

    parse updated

--- parse.c
+++ parse.c
@@ -3,36 +3,41 @@
 /**
  * parse_line - Divise la ligne de commande en tokens.
  * @line: La ligne de commande entrée par l'utilisateur.
- * 
- * Return: Un tableau de chaînes de caractères, chaque élément étant un token.
+ * Return: Un tableau de chaînes de caractères,
+ * chaque élément étant un token.
  */
+
 char **parse_line(char *line)
 {
-    int bufsize = 64, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
+	int bufsize = 64, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-    if (!tokens) {
-        fprintf(stderr, "hsh: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+	if (!tokens)
+	{
+		fprintf(stderr, "hsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-    token = strtok(line, " \t\r\n\a");
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
 
-        if (position >= bufsize) {
-            bufsize += 64;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "hsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "hsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
 
-        token = strtok(NULL, " \t\r\n\a");
-    }
-    tokens[position] = NULL;
-    return tokens;
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
