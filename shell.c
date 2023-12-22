d6bd515a08d91f31840020407da3711b3b8675a3
SpeedCash <tcravero@gmail.com>
2023-12-22 08:13:51 +0000

    shell updated

--- shell.c
+++ shell.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- 	* main - Main loop, receive input from CLI, parse, and execute it
- 	 * @argv: Array of arguments
- 	* @argc: Number of arguments passed to the programmm
-	* Return: Always 0 on success
+ * main - Main loop, receive input from CLI, parse, and execute it
+ * @argv: Array of arguments
+ * @argc: Number of arguments passed to the program
+ * Return: Always 0 on success
  */
-
 int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
 	int status_return = 1, exit_status = 0;
@@ -18,41 +17,26 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 	while (status_return && n != EOF)
 	{
 		size = 0;
-
-
 		status_return = isatty(STDIN_FILENO);
 		if (status_return)
 			write(STDOUT_FILENO, "dav_shell->$ ", 13);
 
-
-		n = getline(&input_stdin, &size, stdin);
+		n = get_input(&input_stdin, &size);
 		if (n == -1)
-		{
-			free(input_stdin);
-			break;
-		}
-
+		break;
 
 		if (validate_spaces(input_stdin))
-		{
-			free(input_stdin);
-			continue;
-		}
-
+		continue;
 
 		arguments = hsh_tokenizer(input_stdin);
-
-
 		if (*arguments[0] == '\0')
-			continue;
+		continue;
 
+		status_return = hsh_execute_builtins
+		(arguments, input_stdin, argv, &exit_status);
 
-		status_return = hsh_execute_builtins(arguments, input_stdin,
-		argv, &exit_status);
-
-		free(arguments);
-		free(input_stdin);
+		free_resources(arguments, input_stdin);
 	}
 
-	return (0);
+	return (exit_status);
 }
